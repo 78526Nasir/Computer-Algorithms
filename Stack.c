075186ac78fe298a27710161516a52b7c09cf00b
78526Nasir <islam35-1102@diu.edu.bd>
2017-07-19 01:52:45 +0600

    final commit

--- Stack.c
+++ Stack.c
@@ -1,89 +1,140 @@
-// Stack Implementation in C using Array.
-
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h> //exit()
+#include<stdbool.h>
+
 #define max 5
 
 int stack[max], top=-1;
 
 void push();
 int pop();
-void disp();
 
-main()
+void pip(); //display()
+void peek();
+bool isFull();
+bool isEmpty();
+int getSize();
+
+int main()
 {
-    int w,r;
+    int choice;
     for(;;)
     {
-        printf("\n1. Push");
-        printf("\n2. Pop");
-        printf("\n3. Display the Stack ");
-        printf("\n4. EXIT");
-        printf("\n\nEnter what you want:");
-        scanf("%d",&w);
-        switch(w)
+
+        printf("\n1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Pip or Display\n");
+        printf("4. Peek\n");
+        printf("5. GetSize\n");
+        printf("6. Exit\n");
+        printf("Please Choose What you Want?: ");
+        scanf("%d",&choice);
+
+        switch(choice)
         {
-        case 1:
-            push();
-            break;
-        case 2:
-            r=pop();
-            break;
-        case 3:
-            disp();
-            break;
-        case 4:
-            exit(1);
-        default:
-            printf("\nInvalid Choice !!\n");
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                pip();
+                break;
+            case 4:
+                peek();
+                break;
+            case 5:
+                printf("\nStack Size is : %d\n",getSize());
+                break;
+            case 6:
+                exit(1);
+            default:
+                printf("\nInvalid Choice!\n");
         }
     }
+    return 0;
 }
+
 
 void push()
 {
-    int num;
-    if(top==max-1)
+    int item;
+    if(isFull())
     {
-        printf("\nStack is Full !\n");
-        return;
+        printf("\nStack is Full!\n");
     }
-    else {
-        printf("\nEnter a number for Insert:");
-        scanf("%d",&num);
+    else
+    {
+        printf("\nEnter stack item: ");
+        scanf("%d",&item);
         top++;
-        stack[top]=num;
+
+        stack[top]=item;
+        printf("\n%d was pushed\n",stack[top]);
     }
 }
 
 int pop()
 {
-    int e;
-    if(top==-1)
+    int item;
+    if(isEmpty())
     {
-        printf("\nStack is Empty !!\n");
+        printf("\nStack is Empty!\n");
         return;
     }
-    else {
-        e=stack[top];
-        printf("\n%d was Deleted !\n",e);
+    else
+    {
+        item=stack[top];
         top--;
-        return e;
-    }
+        printf("\n%d was popped\n",item);
+        return item;
 
+    }
 }
 
-void disp()
+void pip()
 {
     int i;
-    if(top==-1){
-        printf("\nNothing to Display !!\n");
-        return;
+    if(isEmpty())
+    {
+        printf("\nStack is Empty!\n");
     }
-    else {
-        printf("\n");
+    else{
         for(i=top;i>=0;i--)
             printf("\n%d",stack[i]);
         printf("\n");
     }
+}
+
+bool isFull()
+{
+    if(top==max-1)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool isEmpty()
+{
+    if(top==-1)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void peek()
+{
+    printf("\n%d\n",stack[top]);
+}
+
+int getSize()
+{
+    return top+1;
 }
